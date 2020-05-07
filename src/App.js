de29e39af39a5bcb1c486b1bd04739414fe41c90
Clarissa Diesinger <clarissa.diesinger@outlook.com>
2020-05-07 15:10:06 +0200

    Added game logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    const valuesArray = this.makeNewQuestion()
+    this.state = {
+      value1: valuesArray[0],
+      value2: valuesArray[1],
+      value3: valuesArray[2],
+      proposedAnswer: valuesArray[3],
+      numCorrect: 0,
+      numQuestions: 0,
+    }
+  }
+
+  makeNewQuestion = () => {
+    const value1 = Math.floor(Math.random() * 100)
+    const value2 = Math.floor(Math.random() * 100)
+    const value3 = Math.floor(Math.random() * 100)
+    const proposedAnswer =
+      Math.floor(Math.random() * 3) + (value1 + value2 + value3)
+    return [value1, value2, value3, proposedAnswer]
+  }
+
+  updateState = (newValuesArray) => {
+    this.setState((currState) => ({
+      value1: newValuesArray[0],
+      value2: newValuesArray[1],
+      value3: newValuesArray[2],
+      proposedAnswer: newValuesArray[3],
+    }))
+  }
+
+  evaluateAnswer = (givenAnswer) => {
+    const correctAnswer =
+      this.state.value1 + this.state.value2 + this.state.value3
+
+    if (
+      (correctAnswer === this.state.proposedAnswer && givenAnswer === 'true') ||
+      (correctAnswer !== this.state.proposedAnswer && givenAnswer === 'false')
+    ) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  handleClick = (event) => {
+    const answerWasCorrect = this.evaluateAnswer(event.target.name)
+    if (answerWasCorrect) {
+      this.setState((currentState) => ({
+        numCorrect: currentState.numCorrect + 1,
+      }))
+    }
+    this.setState((currentState) => ({
+      numQuestions: currentState.numQuestions + 1,
+    }))
+    this.updateState(this.makeNewQuestion())
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,17 +71,21 @@ class App extends Component {
         <div className="game">
           <h2>Mental Math</h2>
           <div className="equation">
-            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
+            <p className="text">{`${this.state.value1} + ${this.state.value2} + ${this.state.value3} = ${this.state.proposedAnswer}`}</p>
           </div>
-          <button>True</button>
-          <button>False</button>
+          <button name="true" onClick={this.handleClick}>
+            True
+          </button>
+          <button name="false" onClick={this.handleClick}>
+            False
+          </button>
           <p className="text">
-            Your Score: {numCorrect}/{numQuestions}
+            Your Score: {this.state.numCorrect}/{this.state.numQuestions}
           </p>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
